abc31c77ac7c46e6762d46edbc6aeeecd20947cd
Marquis Sampson <marquis4484@gmail.com>
2022-10-27 08:49:26 -0400

    Song Changes

--- components/Center.js
+++ components/Center.js
@@ -34,9 +34,9 @@ function Center() {
 }
 
   return (
-    <div className="flex flex-grow text-white">
+    <div className="flex flex-grow h-screen over-y-scroll scrollbar-hide ">
       <header className='absolute top-5 right-8'>
-        <div className="flex items-center bg-black space-x-3 opacity-90
+        <div className="flex items-center text-white bg-black space-x-3 opacity-90
         hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
             <img src={session?.images} className='rounded-full w-10 h-10' src={session?.userimage}  alt=""/>
             <h2>{session?.user.name}</h2>
--- components/Song.js
+++ components/Song.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
-function Song() {
+function Song({order,track}) {
+    const spotifyApi = useSpotify();
   return (
-    <div>Song</div>
+    <div>
+        <div className='flex items-center space-x-4'>
+            <p>{order + 1}</p>
+            <img className='h-10 w-10' src={track.track.album.images[0].url} alt=""> </img>
+        </div>
+        <div>
+            <p>{track.track.name}</p>
+            <p>{track.track.artists[0].name}</p>
+        </div>
+
+        <div className='flex items center justify-between ml-auto md:ml-0'>
+            <p className='hidden md:inline'>{track.track.album.name}</p>
+            <p>{millisToMinutesAndSeconds()}</p>
+        </div>
+    </div>
   )
 }
 
